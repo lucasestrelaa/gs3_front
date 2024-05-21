41675c1987230cb22a615bd1b8b2fd4e9525dc44
STATION955\Analista19 <analista19.ti@connectsa.com.br>
2024-05-21 16:59:01 -0300

    login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -11,30 +11,35 @@ import { Router } from '@angular/router';
 export class LoginPage implements OnInit {
   public email: string = '';
   public password: string = '';
-  private apiUrl: string = 'http://localhost:3001';
-  constructor(
-    private router: Router,
-    public http: HttpClient
-  ) {
-    
-  }
+  private apiUrl: string = 'http://127.0.0.1:8000/api/';
+  constructor(private router: Router, public http: HttpClient) {}
 
   ngOnInit() {}
   autenticacao = false;
   login() {
     alert('Teste email: ' + this.email + ' senha: ' + this.password);
-    var usuario = { email: this.email, password: this.password };
-    //this.http.post(`${this.apiUrl}/usuario`, usuario).subscribe(
-      //(res) => {
-        this.router.navigate(['/folder/home'])
-      //},
-      //(erro) => {
-      //  if (erro.status == 400) {
-     //     console.log(erro);
-     //   }
-      //}
-   // );
+    var usuario = { login: this.email, password: this.password };
+    this.http.post(`${this.apiUrl}login`, usuario).subscribe(
+      
+      (res) => {
+        console.log("logou")
+        const array = Object.entries(res).map(([chave, valor]) => valor);
+        console.log(array[2]); 
+
+        sessionStorage.setItem('token', array[2])
+        this.router.navigate(['/folder/home']);
+      },
+      (erro) => {
+        if (erro.status == 404) {
+          console.log(erro);
+        }
+      }
+    );
   }
-  cadastrar() {}
-  logout() {}
+  cadastrar() {
+
+  }
+  logout() {
+
+  }
 }
